c69773b4f2ad3a1e1b4fe093058765a74f1e690f
TingHuanClay <claytinghuan@gmail.com>
2020-02-29 17:32:20 +0800

    refactor: remove redundant parameter of method

--- Problems_and_Solutions/0046_permutations/Solution.java
+++ Problems_and_Solutions/0046_permutations/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(res, new ArrayList<Integer>(), nums, 0);
+        backtrack(res, new ArrayList<Integer>(), nums);
         return res;
     }
-    
-    private void backtrack(List<List<Integer>> res, List<Integer> tmp, int[] nums, int start) {
+
+    private void backtrack(List<List<Integer>> res, List<Integer> tmp, int[] nums) {
         if (tmp.size() == nums.length) {
             res.add(new ArrayList<Integer>(tmp));
             return;
         }
-        for (int i = 0 ; i < nums.length ; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (!tmp.contains(nums[i])) {
                 tmp.add(nums[i]);
-                backtrack(res, tmp, nums, i + 1);
+                backtrack(res, tmp, nums);
                 tmp.remove(tmp.size() - 1);
             }
         }
